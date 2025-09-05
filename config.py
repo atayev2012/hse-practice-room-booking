434e1b91883f2cf7246cb6500bd61fe625f85d15
Batyr Atayev <atayev2012@gmail.com>
2025-09-05 15:43:18 +0300

    Added Google table url and Google credentials.json path

--- config.py
+++ config.py
@@ -4,8 +4,10 @@ from dotenv import load_dotenv
 load_dotenv()
 
 class Config:
-    TOKEN = os.getenv("TOKEN")
-    REPORT_CHAT_ID = os.getenv("REPORT_CHAT_ID")
+    TELEGRAM_TOKEN = os.getenv("TELEGRAM_TOKEN")
+    TELEGRAM_REPORT_CHAT_ID = os.getenv("TELEGRAM_REPORT_CHAT_ID")
     DB_URL = os.getenv("DB_URL")
+    GOOGLE_TABLE_URL=os.getenv("GOOGLE_TABLE_URL")
+    GOOGLE_CREDENTIALS_PATH=os.getenv("GOOGLE_CREDENTIALS_PATH")
 
 config = Config()
