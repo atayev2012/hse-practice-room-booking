be4ee28febafe979417377a2be2b0b6e6f15931b
Batyr Atayev <atayev2012@gmail.com>
2025-09-09 16:39:49 +0300

    Added is_upper_week() to identify if target date is in upper week or not

--- google/schedule_parser.py
+++ google/schedule_parser.py
@@ -1,4 +1,6 @@
 from gspread import Spreadsheet
+from datetime import date, timedelta
+from config import config
 
 data_dict = {}
 
@@ -39,6 +41,37 @@ async def load_and_parse(spreadsheet: Spreadsheet):
         # print(f"{worksheet.title} => {worksheet_data}")
         # print(worksheet.title)
         # print(worksheet.get_all_values())
+
+
+# Verify if week is upper (else it is lower)
+async def is_upper_week(
+        target_date: date,
+        education_start_date: date = config.MODULE_I_START,
+        education_end_date: date = config.MODULE_IV_END
+) -> bool | None:
+    """
+    :param target_date: The date to be checked if it is in an upper week
+    :param education_start_date: The date indicating start of education
+    :param education_end_date: The date indicating end of education
+
+    :return: 'True' if it is upper week, 'False' otherwise (None in case of date validation error)
+    """
+
+    if not isinstance(target_date, date) or target_date < education_start_date or target_date > education_end_date:
+        return None
+
+    education_start_weekday = education_start_date.weekday()
+
+    # If first week starts with any day above 5 (Saturday) then move higher week to next week
+    # else make higher week with current week by moving date to Monday of the same week
+    if education_start_weekday > 5:
+        first_week_start_date = education_start_date + timedelta(days=7 - education_start_weekday)
+    else:
+        first_week_start_date = education_start_date - timedelta(days=education_start_weekday)
+
+    # calculate date difference between two dates + calculate weeks + identify if even or odd
+    time_diff = target_date - first_week_start_date
+    return (time_diff.days // 7) % 2 == 0
 
 
 if __name__ == '__main__':
