76b9c2c17606ff4a6da099975bab33a61e77809b
Batyr Atayev <atayev2012@gmail.com>
2025-09-22 17:31:10 +0300

    Changed MODULE start and end date storage way

--- config.py
+++ config.py
@@ -17,21 +17,29 @@ class Config:
     GOOGLE_CREDENTIALS_PATH = os.getenv("GOOGLE_CREDENTIALS_PATH")
 
     # Education modules start and end dates
-    MODULE_I_START = datetime.strptime(str(os.getenv("MODULE_I_START")),"%d.%m.%Y").date()
-    MODULE_I_END = datetime.strptime(str(os.getenv("MODULE_I_END")),"%d.%m.%Y").date()
-    MODULE_II_START = datetime.strptime(str(os.getenv("MODULE_II_START")), "%d.%m.%Y").date()
-    MODULE_II_END = datetime.strptime(str(os.getenv("MODULE_II_END")),"%d.%m.%Y").date()
-    MODULE_III_START = datetime.strptime(str(os.getenv("MODULE_III_START")),"%d.%m.%Y").date()
-    MODULE_III_END = datetime.strptime(str(os.getenv("MODULE_III_END")),"%d.%m.%Y").date()
-    MODULE_IV_START = datetime.strptime(str(os.getenv("MODULE_IV_START")),"%d.%m.%Y").date()
-    MODULE_IV_END = datetime.strptime(str(os.getenv("MODULE_IV_END")),"%d.%m.%Y").date()
+    MODULES = {
+        "1": {
+            "start": datetime.strptime(str(os.getenv("MODULE_I_START")),"%d.%m.%Y").date(),
+            "end": datetime.strptime(str(os.getenv("MODULE_I_END")),"%d.%m.%Y").date()
+        },
+        "2": {
+            "start": datetime.strptime(str(os.getenv("MODULE_II_START")), "%d.%m.%Y").date(),
+            "end": datetime.strptime(str(os.getenv("MODULE_II_END")),"%d.%m.%Y").date()
+        },
+        "3": {
+            "start": datetime.strptime(str(os.getenv("MODULE_III_START")),"%d.%m.%Y").date(),
+            "end": datetime.strptime(str(os.getenv("MODULE_III_END")),"%d.%m.%Y").date()
+        },
+        "4": {
+            "start": datetime.strptime(str(os.getenv("MODULE_IV_START")),"%d.%m.%Y").date(),
+            "end": datetime.strptime(str(os.getenv("MODULE_IV_END")),"%d.%m.%Y").date()
+        }
+    }
 
 config = Config()
 
 if __name__ == "__main__":
-    print(f"MODULE 1 = START: {config.MODULE_I_START} | END: {config.MODULE_I_END}")
-    print(f"MODULE 2 = START: {config.MODULE_II_START} | END: {config.MODULE_II_END}")
-    print(f"MODULE 3 = START: {config.MODULE_III_START} | END: {config.MODULE_III_END}")
-    print(f"MODULE 4 = START: {config.MODULE_IV_START} | END: {config.MODULE_IV_END}")
+    print(f"MODULE 1 = START: {config.MODULES['1']['start']} | END: {config.MODULES['1']['end']}")
+
     # print(config.MODULE_I_END)
     # print(type(config.MODULE_III_START))
