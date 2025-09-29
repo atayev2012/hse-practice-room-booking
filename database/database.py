178e33202920fc0d6372c0cabcb8c22e4678b332
Batyr Atayev <atayev2012@gmail.com>
2025-09-29 04:13:22 +0300

    Turned off echo on engine

--- database/database.py
+++ database/database.py
@@ -4,7 +4,7 @@ from sqlalchemy.orm import sessionmaker, DeclarativeBase, Mapped, mapped_column
 from datetime import datetime
 from config import config
 
-engine = create_async_engine(config.DB_URL, echo=True)
+engine = create_async_engine(config.DB_URL, echo=False)
 async_session_maker = sessionmaker(engine, class_=AsyncSession, expire_on_commit=False)
 
 class Base(AsyncAttrs, DeclarativeBase):
