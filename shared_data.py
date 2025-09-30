68b807e0075db66534f2824adc4b29ddf4cc2ffd
Batyr Atayev <atayev2012@gmail.com>
2025-09-30 08:15:38 +0300

    Added get_room_list and get_buildings_dict

--- shared_data.py
+++ shared_data.py
@@ -21,4 +21,35 @@ async def update_schedule():
                     SCHEDULE_SHARED[building][room] = {}
                     for category in temp_data[building][room].keys():
                         SCHEDULE_SHARED[building][room][category] = copy.deepcopy(temp_data[building][room][category])
+        print("Schedule was updated")
         await asyncio.sleep(120)
+
+
+# list of rooms
+async def get_rooms_list(building_code: str) -> list:
+    async with LOCK:
+        rooms = list(SCHEDULE_SHARED.get(building_code).keys())
+    return rooms
+
+
+# get buildings by codes with their addresses if available
+async def get_buildings_dict() -> dict:
+    buildings = {
+        "сормово": "Сормовское ш., 30",
+        "родионова": "ул. Родионова, 136",
+        "львовская": "ул. Львовская, 1в",
+        "б.печерская": "ул. Большая Печерская, 25/12",
+        "костина": "ул. Костина, 2",
+    }
+    async with LOCK:
+        building_codes = list(SCHEDULE_SHARED.keys())
+
+    result_buildings = {}
+    for item in building_codes:
+        building_code = item.lower()
+        if buildings.get(building_code):
+            result_buildings[building_code] = buildings.get(building_code)
+        else:
+            result_buildings[building_code] = item
+
+    return result_buildings
